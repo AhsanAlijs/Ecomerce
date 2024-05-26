fc39bb7db081562339916206073a706ef1647add
ahsanalijs <ahsanali45444@gmail.com>
2024-05-26 16:22:53 +0500

    Navbar and hero section is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,16 +107,17 @@ const Navbar = () => {
                 </ul>
             </article>
 
-            <article className='bg-[#F4F4F4] flex items-center justify-evenly  ' >
+            <article className='bg-[#F4F4F4] flex items-center justify-between flex-wrap p-4' >
 
-                <div onMouseEnter={() => setMousehover(true)}
-                    className=' flex gap-6 bg-[#EDA415] p-4 text-white w-fit  ' >
-                    <span  >Browse categories</span>
-                    <span  ><RiArrowDownSFill size={30} /></span>
+                <div
+                    onMouseEnter={() => setMousehover(true)}
+                    className='flex gap-6 bg-[#EDA415] p-4 text-white w-full md:w-auto justify-between md:justify-center'
+                >
+                    Browse categories
+                    <RiArrowDownSFill size={30} />
                 </div>
 
-
-                <ul className='flex items-center gap-6 ' >
+                <ul className='flex flex-wrap items-center gap-6 mt-4 md:mt-0'>
                     <li className='flex items-center justify-center'>
                         <span className='flex gap-2'>
                             Home
@@ -124,19 +125,19 @@ const Navbar = () => {
                         </span>
                     </li>
                     <li className='flex items-center justify-center'>
-                        <span className='flex gap-4' >
+                        <span className='flex gap-4'>
                             Catalog
                             <span><RiArrowDownSFill size={30} /></span>
                         </span>
                     </li>
                     <li className='flex items-center justify-center'>
-                        <span className='flex gap-4' >
+                        <span className='flex gap-4'>
                             Blog
                             <span><RiArrowDownSFill size={30} /></span>
                         </span>
                     </li>
                     <li className='flex items-center justify-center'>
-                        <span className='flex gap-4' >
+                        <span className='flex gap-4'>
                             Pages
                             <span><RiArrowDownSFill size={30} /></span>
                         </span>
@@ -149,22 +150,21 @@ const Navbar = () => {
                     </li>
                 </ul>
 
-                <div className='text-lg font-bold text-[#003F62] ' >
+                <div className='text-lg font-bold text-[#003F62] mt-4 md:mt-0'>
                     30 Days Free Return
                 </div>
 
             </article>
 
-            <ul className={`bg-[#F4F4F4] absolute rounded-lg shadow-xl flex items-start w-fit p-4 top-[218px] right-[977px] flex-col transform transition-all duration-300 ease-in-out ${mouseHover ? 'opacity-100 translate-y-15 cursor-pointer ' : 'opacity-0 -translate-y-[100%]'
-                }`}
-            >
-                <a href='#' >Laptops</a>
-                <a href='#' >Mobiles</a>
-                <a href='#' >Grocery</a>
-                <a href='#' >Widgets</a>
-                <a href='#' >Men Collection Brands</a>
-                <a href='#' >Women Collection Brands</a>
+            <ul className={`bg-[#F4F4F4] absolute rounded-lg shadow-xl flex items-start w-fit p-4 top-[230px] right-[20px] md:right-[700px] lg:right-[1039.8px] flex-col transform transition-all duration-300 ease-in-out  ${mouseHover ? 'opacity-100 translate-y-15 cursor-pointer ' : 'opacity-0 -translate-y-[100%]'}`}>
+                <a className='hover:text-[#003F62]' href='#'>Laptops</a>
+                <a className='hover:text-[#003F62]' href='#'>Mobiles</a>
+                <a className='hover:text-[#003F62]' href='#'>Grocery</a>
+                <a className='hover:text-[#003F62]' href='#'>Widgets</a>
+                <a className='hover:text-[#003F62]' href='#'>Men Collection Brands</a>
+                <a className='hover:text-[#003F62]' href='#'>Women Collection Brands</a>
             </ul>
+
 
         </main >
     );
--- src/screen/Home/Home.jsx
+++ src/screen/Home/Home.jsx
@@ -3,26 +3,49 @@ import React from 'react'
 const Home = () => {
     return (
 
-        <main className='flex flex-col md:flex-row items-center justify-around bg-white p-4 md:p-8'>
-            <div className='flex items-start flex-col mb-6 md:mb-0'>
-                <span className='text-[#1B5A7D] font-bold text-4xl'>Canon</span>
-                <span className='text-[#1B5A7D] font-bold text-4xl'>camera</span>
-                <div className='flex mt-4 items-center gap-4 md:gap-10'>
-                    <button className='text-white bg-[#EDA415] rounded-xl px-6 py-4'>
-                        Shop now
-                    </button>
-                    <button className='text-[#316887] border-[#316887] border rounded-xl px-6 py-4'>
-                        View more
-                    </button>
+        <main>
+            <article className='flex flex-col md:flex-row items-center justify-around bg-white p-4 md:p-8'>
+                <div className='flex items-start flex-col mb-6 md:mb-0'>
+                    <span className='text-[#1B5A7D] font-bold text-4xl'>Canon</span>
+                    <span className='text-[#1B5A7D] font-bold text-4xl'>camera</span>
+                    <div className='flex mt-4 items-center gap-4 md:gap-10'>
+                        <button className='text-white bg-[#EDA415] rounded-xl px-6 py-4'>
+                            Shop now
+                        </button>
+                        <button className='text-[#316887] border-[#316887] border rounded-xl px-6 py-4'>
+                            View more
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className='relative'>
-                <img src="/bnner.png" alt="camera" className='w-full md:w-auto' />
-                <div className='absolute flex flex-col px-4 py-3 md:px-[50px] md:py-[45px] top-[75%] md:top-[200px] right-[10%] md:left-[200px] bg-[#EDA415] rounded-full text-lg md:text-xl font-semibold text-white'>
-                    <span>Only</span>
-                    <span>$120</span>
+                <div className='relative'>
+                    <img src="/bnner.png" alt="camera" className='w-full md:w-auto ' />
+                    <div className='absolute flex flex-col px-4 py-3 md:px-[50px] md:py-[45px] top-[60%] md:top-[200px] right-[10%] md:right-[-30px] bg-[#EDA415] rounded-full text-lg md:text-xl font-semibold text-white'>
+                        <span>Only</span>
+                        <span>$120</span>
+                    </div>
                 </div>
-            </div>
+            </article>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </main>
 
 
